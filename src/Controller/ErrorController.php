e5d41ec3b1ccbfbd3c83bf1de4c20b6a7025d81c
Abdou Karim Coulibaly <98843343+absrimka@users.noreply.github.com>
2023-05-05 21:24:55 +0000

    access_control

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,19 @@
+<?php
+// src/Controller/ErrorController.php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ErrorController extends AbstractController
+{
+    public function error404()
+    {
+        return $this->render('bundles/TwigBundle/Exception/404.html.twig', [
+            'controller_name' => 'ErrorController',
+        ]);
+        /*throw new NotFoundHttpException('Cette page n\'existe pas.');*/
+    }
+}
